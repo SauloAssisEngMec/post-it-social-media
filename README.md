79b09ac68f100c97c0844c5cc651c2dd15fad0b1
saulo <sauloassis4123@gmail.com>
2024-06-04 19:37:21 -0300

    using libriries for validate input filed stead of middlares from scratch

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Post It Social Media
+# PostIt Social Media
+
+#### Post it as App tha allows people post everything and share your ideas with others guys.
+
+- This backend app will be doing with Test driven develpment (TDD) using jest
+- we will use for validation input: validation middlware (from scratch) and with express-validator to validate email format etc to practice validate in backend stead of frontend, as commonly used.
+- .
+  .
+  .
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -2,42 +2,41 @@ const express = require('express');
 const UserService = require('./UserService');
 
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-const validateUserName = (req, res, next) => {
-  const user = req.body;
-  if (user.userName === null) {
-    req.validationErrors = {
-      userName: 'userName cant be null',
-    };
-    // return res
-    //   .status(400)
-    //   .send({ validationErrors: { userName: 'userName cant be null' } });
-  }
-  next();
-};
+// const validateUserName = (req, res, next) => {
+//   const user = req.body;
+//   if (user.userName === null) {
+//     req.validationErrors = {
+//       userName: 'userName cant be null',
+//     };
+//   }
+//   next();
+// };
 
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'E-mail cant be null',
-    };
-    // return res
-    //   .status(400)
-    //   .send({ validationErrors: { email: 'E-mail cant be null' } });
-  }
-  next();
-};
+// const validateEmail = (req, res, next) => {
+//   const user = req.body;
+//   if (user.email === null) {
+//     req.validationErrors = {
+//       ...req.validationErrors,
+//       email: 'E-mail cant be null',
+//     };
+//   }
+//   next();
+// };
 
 router.post(
   '/api/v1/users',
-  validateUserName,
-  validateEmail,
+  check('userName').notEmpty().withMessage('userName cant be null'),
+  check('email').notEmpty().withMessage('E-mail cant be null'),
   async (req, res) => {
-    if (req.validationErrors) {
-      const response = { validationErrors: { ...req.validationErrors } };
-      return res.status(400).send(response);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors
+        .array()
+        .forEach((error) => (validationErrors[error.path] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
     }
     await UserService.save(req.body);
     return res.send({ message: 'success message' });
