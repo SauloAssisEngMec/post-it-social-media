02e276cdfae21e9f1424d44974f756eef5392f7b
saulo <sauloassis4123@gmail.com>
2024-06-05 23:13:57 -0300

    test: implementing dinamic test for similar one

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -21,9 +21,9 @@ beforeEach(() => {
 
 describe('Register: User', () => {
   test('it should return status code 200 when singup request is valid', async () => {
-    // 1  way
+    // first  way (with then and done)
     //request(app).get('/api/v1/users').send(defaultUser).expect(200, done);
-    // 2  way
+    // 2 second  way (with then and done)
     // request(app)
     //   .post('/api/v1/users')
     //   .send(defaultUser)
@@ -31,7 +31,7 @@ describe('Register: User', () => {
     //     expect(response.status).toBe(200);
     //     done();
     //   });
-    // 3  way
+    // third  way (with async awy - recomended)
     const response = await request(app).post('/api/v1/users').send(defaultUser);
 
     expect(response.status).toBe(200);
@@ -86,24 +86,6 @@ describe('Register: User', () => {
     expect(body.validationErrors).not.toBeUndefined();
   });
 
-  test('it should return userName cant be null when userName was null', async () => {
-    const nullUser = Object.assign({}, defaultUser, { userName: null });
-
-    const response = await request(app).post('/api/v1/users').send(nullUser);
-    const body = response.body;
-
-    expect(body.validationErrors.userName).toBe('userName cant be null');
-  });
-
-  test('it should return E-mail cant be null when E-Mail was null', async () => {
-    const nullEmail = Object.assign({}, defaultUser, { email: null });
-
-    const response = await request(app).post('/api/v1/users').send(nullEmail);
-    const body = response.body;
-
-    expect(body.validationErrors.email).toBe('E-mail cant be null');
-  });
-
   test('it should return errors for both E-mail and userName when both are null', async () => {
     const bothNull = Object.assign({}, defaultUser, {
       userName: null,
@@ -114,5 +96,63 @@ describe('Register: User', () => {
     const body = response.body;
 
     expect(Object.keys(body.validationErrors)).toEqual(['userName', 'email']);
+  });
+
+  // test('it should return userName cant be null when userName was null', async () => {
+  //   const nullUser = Object.assign({}, defaultUser, { userName: null });
+
+  //   const response = await request(app).post('/api/v1/users').send(nullUser);
+  //   const body = response.body;
+
+  //   expect(body.validationErrors.userName).toBe('userName cant be null');
+  // });
+
+  // test('it should return E-mail cant be null when E-Mail was null', async () => {
+  //   const nullEmail = Object.assign({}, defaultUser, { email: null });
+
+  //   const response = await request(app).post('/api/v1/users').send(nullEmail);
+  //   const body = response.body;
+
+  //   expect(body.validationErrors.email).toBe('E-mail cant be null');
+  // });
+
+  // test('it should return Password cant be null when password is null', async () => {
+  //   const nullPassword = Object.assign({}, defaultUser, { password: null });
+
+  //   const response = await request(app)
+  //     .post('/api/v1/users')
+  //     .send(nullPassword);
+  //   const body = response.body;
+
+  //   expect(body.validationErrors.password).toBe('Password cant be null');
+  // });
+
+  // implementing dinamic test for similar test  with (test.each(table)(name, fn, timeout))
+
+  // test.each([
+  //   ['userName', 'userName cant be null'],
+  //   ['email', 'E-mail cant be null'],
+  //   ['password', 'Password cant be null'],
+  // ])('When %s is null then %s is a response', async (field, expected) => {
+  //   const nullField = (defaultUser[field] = null);
+
+  //   const response = await request(app).post('/api/v1/users').send(nullField);
+  //   const body = response.body;
+
+  //   expect(body.validationErrors[field]).toBe(expected);
+  // });
+
+  // implementing dinamic test with (test.each`table`(name, fn, timeout))
+
+  test.each`
+    field         | expected
+    ${'userName'} | ${'userName cant be null'}
+    ${'email'}    | ${'E-mail cant be null'}
+    ${'password'} | ${'Password cant be null'}
+  `('returns $expected when $field is null', async ({ field, expected }) => {
+    const nullField = (defaultUser[field] = null);
+    const response = await request(app).post('/api/v1/users').send(nullField);
+    const body = response.body;
+    expect(body.validationErrors[field]).toBe(expected);
   });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -29,6 +29,7 @@ router.post(
   '/api/v1/users',
   check('userName').notEmpty().withMessage('userName cant be null'),
   check('email').notEmpty().withMessage('E-mail cant be null'),
+  check('password').notEmpty().withMessage('Password cant be null'),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
